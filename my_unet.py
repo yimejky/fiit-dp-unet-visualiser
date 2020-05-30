a1df41c38fee08b0c600338a1742e47d1ac48dcf
Nikolas Tomastik <nikolas.tsk@gmail.com>
2020-05-30 20:26:13 +0200

    added input and sigmod

--- my_unet.py
+++ my_unet.py
@@ -9,10 +9,15 @@ def block(conv_num, pool_num, in_channels, out_channels, height, height_out, to=
     pn = pool_num
 
     if to is None:
+        offset = "(1,0,0)"
+    else:
+        offset = "(0,0,0)"
+
+    if to is None:
         to = f"(pool{pn-1}-east)"
 
     return [
-        to_Conv(f"conv{cn}", '', in_channels, offset="(1,0,0)", to=to, height=height, depth=height, width=width),
+        to_Conv(f"conv{cn}", '', in_channels, offset=offset, to=to, height=height, depth=height, width=width),
         to_Conv(f"conv{cn+1}", '', out_channels, offset="(0,0,0)", to=f"(conv{cn}-east)", height=height, depth=height, width=width*width_scaller),
         to_Pool(f"pool{pn}", offset="(0,0,0)", to=f"(conv{cn+1}-east)", height=height_out, depth=height_out, width=1),
     ]
@@ -35,12 +40,14 @@ arch = [
     to_head( '..' ),
     to_cor(),
     to_begin(),
+
+    to_input( './input.png' ),
 ]
 
 in_ch = 16
 width = 1
 max_num = 4
-width_scaller = 1.7
+width_scaller = 1.65
 height_init = 6
 
 max_width = width * width_scaller ** max_num
@@ -70,7 +77,8 @@ for i in range(max_num):
     arch.extend(unblock(1 + i*2, j, tmp_in_ch, tmp_out_ch, height=height_init*(j+1), to=to, width=tmp_width, width_scaller=width_scaller))
 
 arch.extend([
-    to_Conv("output", '', 1, offset="(1,0,0)", to=f"(unconv8-east)", height=max_height, depth=max_height, width=width),
+    to_Conv("output", '', 1, offset="(1,0,0)", to=f"(unconv8-east)", height=max_height, depth=max_height, width=width, caption="sigmoid"),
+    # to_ConvSoftMax("output", 1, "(1,0,0)", to="(unconv8-east)", caption="Sigmoid",  height=max_height, depth=max_height),
 
     to_connection("pool1", "conv3"), 
     to_connection("pool2", "conv5"), 
